1cbc8a31a26db3944861f224f723ba1ab712153a
Mason Hoffman <mhoffman3939@gmail.com>
2021-05-11 10:30:23 -0700

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 import axios from 'axios';
 import EntryForm from './EntryForm.js';
 import GetPatientListButton from './GetPatientListButton.js';
 import PatientList from './PatientList.js';
-import NavbarComp from './NavbarComp.js';
+import Navigation from './Navigation.js';
+import Home from './Home.js';
 
 const App = () => {
   const [patientList, setPatientList] = useState();
@@ -40,19 +50,53 @@ const App = () => {
   }
 
   return (
-    <div>
-      <NavbarComp />
-      <h1 className="company">Sound Dental Care</h1>
-      <div className="container">
-        <div>
-          <EntryForm savePatient={savePatient}/>
-        </div>
-        <div>
-          <GetPatientListButton getPatientList={getPatientList} />
-          <PatientList  patientList={patientList}/>
-        </div>
+    <Router>
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand as={Link} to="/">Sound Dental Care</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Item>
+                <Nav.Link as={Link} to="/add">Add</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/list">Patient List</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Switch>
+          <Route path="/add">
+            <div>
+              <EntryForm savePatient={savePatient}/>
+            </div>
+          </Route>
+          <Route path="/list">
+            <div>
+              <GetPatientListButton getPatientList={getPatientList} />
+              <PatientList  patientList={patientList}/>
+            </div>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
+    // <div>
+    //   <Navigation savePatient={savePatient}/>
+    //   <h1 className="company">Sound Dental Care</h1>
+    //   <div className="container">
+    //     <div>
+    //       <EntryForm savePatient={savePatient}/>
+    //     </div>
+    //     <div>
+    //       <GetPatientListButton getPatientList={getPatientList} />
+    //       <PatientList  patientList={patientList}/>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
